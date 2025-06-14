005d25522532f49d82389428ed0e890d12dcfc03
dvaldesg <diego.valdes.g@uc.cl>
2025-06-14 18:15:28 -0400

    fix(build-config): created secret on eas and added as a process.env variable

--- apiConfig.ts
+++ apiConfig.ts
@@ -1,2 +1,5 @@
-import { API_BASE_URL } from "@env";
-export const BASE_URL = API_BASE_URL;
+import Constants from "expo-constants";
+
+export const BASE_URL = Constants.expoConfig?.extra?.API_BASE_URL;
+
+console.log("🧪 API_BASE_URL from Constants:", Constants.expoConfig?.extra?.API_BASE_URL || "🚨 MISSING API_BASE_URL");
---
+++ app.config.js
@@ -0,0 +1,52 @@
+export default ({ config }) => ({
+  ...config,
+  expo: {
+    ...config.expo,
+    name: "KaizenApp",
+    slug: "KaizenApp",
+    scheme: "kaizenapp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#94A9FF"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: "com.ikigai.kaizenapp",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      "expo-secure-store"
+    ],
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    extra: {
+      ...config.expo?.extra,
+      router: {},
+      eas: {
+        projectId: "51436d55-3eaf-45bc-8bac-5351fa94b222"
+      },
+      API_BASE_URL: process.env.API_BASE_URL
+    }
+  }
+});
